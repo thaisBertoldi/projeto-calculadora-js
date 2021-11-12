794813b2c9ea55f5eff38ec420ca72a78ae86cf8
Thais Bertoldi <thais_brt@hotmail.com>
2021-11-12 14:50:14 -0300

    Cria funções 'apagaUm' e 'realizaConta'

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,39 @@ function criaCalculadora() {
             this.display.value = '';
         },
 
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
         inicia() {
             this.clickBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta inválida');
+                    return;
+                }
+
+                this.display.value = conta;
+            } catch(e) {
+                alert('Conta inválida');
+                return;
+            }
         },
 
         clickBotoes() {
@@ -21,6 +52,14 @@ function criaCalculadora() {
 
                 if(el.classList.contains('btn-clear')) {
                     this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
                 }
             });
         },
