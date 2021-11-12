37dc77e7e8c7ce2c7b0ad6f0fb8fd7c1836f71d2
Thais Bertoldi <thais_brt@hotmail.com>
2021-11-12 11:16:34 -0300

    Criada tabela e funcionalidade 'clear'

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        inicia() {
+            this.clickBotoes();
+        },
+
+        clickBotoes() {
+            document.addEventListener('click', e => {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+            });
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        },
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
